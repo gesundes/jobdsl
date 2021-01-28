6810d92e4eff417fe170597733d4416312f321b5
Oleg Lipin <gesundes@gmail.com>
2021-01-28 18:47:39 +0300

    Update MasterSeed.groovy

--- MasterSeed.groovy
+++ MasterSeed.groovy
@@ -3,13 +3,14 @@ def configSlurper = new ConfigSlurper()
 def seedConfig = configSlurper.parse(readFileFromWorkspace("seedConfig.cfg"))
 
 seedConfig.seedJobs.each { name, path ->
-  Closure jobConfig = { readFileFromWorkspace("${path}") }
+  def jobConfig = { readFileFromWorkspace("${path}") }
   assert jobConfig instanceof Closure
   
-/*  def myJob = job("${name}") {
-    readFileFromWorkspace("${path}")
+  def myJob = job("${name}") {
+    jobConfig
   }
 
+/*
   def myJob = job("${name}")
   myJob.with {
     readFileFromWorkspace("${path}")
