db346be523f06f85f1df5e5fe538a87bcbd5660f
Oleg Lipin <gesundes@gmail.com>
2021-01-28 20:50:59 +0300

    Update MasterSeed.groovy

--- MasterSeed.groovy
+++ MasterSeed.groovy
@@ -9,7 +9,7 @@ seedConfig.seedJobs.each { name, path ->
   
   def closure = {
         scm {
-            git("git://github.com/", branchName)
+            git("git://github.com/")
         }
   }
   
