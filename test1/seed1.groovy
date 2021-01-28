64944fe3634eaaab812cb9ef219458e89acab13e
Oleg Lipin <gesundes@gmail.com>
2021-01-28 19:17:49 +0300

    Update seed1.groovy

--- test1/seed1.groovy
+++ test1/seed1.groovy
@@ -1,12 +1,3 @@
-scm {
-    git {
-        remote {
-            name('remoteB')
-            url('git@server:account/repo1.git')
-        }
-        extensions {
-            cleanAfterCheckout()
-            relativeTargetDirectory('repo1')
-        }
-    }
+steps {
+    shell('echo Hello World!')
 }
