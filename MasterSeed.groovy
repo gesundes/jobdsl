2f035cd7900c53958d14c0becdf7f268bebf00d4
Oleg Lipin <gesundes@gmail.com>
2021-01-28 19:05:56 +0300

    Update MasterSeed.groovy

--- MasterSeed.groovy
+++ MasterSeed.groovy
@@ -1,32 +1,8 @@
-def configSlurper = new ConfigSlurper()
-
-def seedConfig = configSlurper.parse(readFileFromWorkspace("seedConfig.cfg"))
-
-seedConfig.seedJobs.each { name, path ->
-  def jobConfig = { readFileFromWorkspace("${path}") }
-  assert jobConfig instanceof Closure
-  
-  job("${name}") {
-    
+job("JobName") {
     scm {
-      git {
-          remote {
-              name('remoteB')
-              url('git@server:account/repo1.git')
-          }
-          extensions {
-              cleanAfterCheckout()
-              relativeTargetDirectory('repo1')
-          }
-      }
+        git("git://github.com/${project}.git", branchName)
     }
-    
-  }
-
-/*
-  def myJob = job("${name}")
-  myJob.with {
-    readFileFromWorkspace("${path}")
-  }
-*/
+    steps {
+        maven("test -Dproject.name=${project}/${branchName}")
+    }
 }
