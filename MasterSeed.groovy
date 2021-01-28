d47b559771fb14b73c46c7c17a928902da7c2b5c
Oleg Lipin <gesundes@gmail.com>
2021-01-28 20:44:04 +0300

    Update MasterSeed.groovy

--- MasterSeed.groovy
+++ MasterSeed.groovy
@@ -6,9 +6,19 @@ seedConfig.seedJobs.each { name, path ->
   println(readFileFromWorkspace("${path}"))
   Closure jobConfig = { readFileFromWorkspace("${path}") }
   println(jobConfig)
+  
+  def content = {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+  }
+  
+  job name content
 
+/*
   def myJob = job("${name}")
   myJob.with {
     this.readFileFromWorkspace("${path}")
   }
+*/
 }
